cb57811294d22cf031c9154059bda055d8f50e71
Michael Pilosov <40366263+mathematicalmichael@users.noreply.github.com>
2024-05-26 17:55:46 -0600

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,30 +1,26 @@
 # Jupyterlab icons can be created here to launch apps
 # that are included in the user's image.
 c.ServerProxy.servers = {
-  'vscode': {
-    'command': [
-		'code-server',
-		'--auth', 'none',
-		'--bind-addr', '0.0.0.0',
-		'--port', '5000'
-	],
-    'port': 5000,
-    'absolute_url': False,
-    'new_browser_tab': True,
-    'timeout': 10,
-    'launcher_entry': {
-        'title': 'VSCode',
-        },
-  },
+ #  'vscode': {
+ #    'command': [
+	# 	'code-server',
+	# 	'--auth', 'none',
+	# 	'--bind-addr', '0.0.0.0',
+	# 	'--port', '5000'
+	# ],
+ #    'port': 5000,
+ #    'absolute_url': False,
+ #    'new_browser_tab': True,
+ #    'timeout': 10,
+ #    'launcher_entry': {
+ #        'title': 'VSCode',
+ #        },
+ #  },
   'app': {
     'command': [
-		'streamlit', 'run', 'app.py',
-		'--server.address', '0.0.0.0',
-		'--server.port', '8080',
-	        '--server.enableCORS', 'False',
-	        '--server.enableXsrfProtection', 'False',
+		'./start.sh'
 	],
-    'port': 8080,
+    'port': 8501,
     'absolute_url': False,
     'new_browser_tab': True,
     'timeout': 10,
